af1cc03908c6bc76247ebb3e7f835cbce307f1bf
Ramonet22400 <ramonperez22400@gmail.com>
2022-09-21 13:37:28 +0200

    P1 PHP

---
+++ EX_01.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <h1> Ejercicio 1</h1>
+    </head>
+    <body>
+
+    <h6> P1 Introducción PHP </h6>
+        <p>
+            <?php
+            echo "Primera página web con PHP";
+            ?>
+        </p>
+    </body>
+</html>
